b0e5876bf3293893b7cfb2e42c268f1b80162572
Ragnall16 <ragnall.malfath@gmail.com>
2024-09-04 14:06:54 +0700

    Initialize PWS

--- gopedia_tugas2_pbp/settings.py
+++ gopedia_tugas2_pbp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g89u97mm!+twdibd&f+ek2@ienzqty)97o34!ync_^ixgrla0!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ragnall-muhammad-gopedia.pbp.cs.ui.ac.id"]
 
 
 # Application definition

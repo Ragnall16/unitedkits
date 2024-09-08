50bcab5f0f9dcc58d29f79286c3962e5def79b3f
Ragnall16 <ragnall.malfath@gmail.com>
2024-09-08 21:07:45 +0700

    Website Modification

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase, Client
+from django.utils import timezone
+from .models import ECommerce
+
+class mainTest(TestCase):
+    def test_main_url_is_exist(self):
+        response = Client().get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_using_main_template(self):
+        response = Client().get('')
+        self.assertTemplateUsed(response, 'main.html')
+
+    def test_nonexistent_page(self):
+        response = Client().get('/skibidi/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_item_availability(self):
+        now = timezone.now()
+        avai = ECommerce.objects.create(
+            name = "20/21 Third Kit",
+            price = 1500000,
+            time = now,
+            description = "Manchester United Third Kit for the 20/21 Season",
+            quantity = 10,
+            sold = 10,
+        )
+        self.assertFalse(avai.is_avai)
--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name' : '23/24 Home Kit',
+        'name' : '24/25 Away Kit',
         'price': '1000000',
-        'description': 'Manchester United Home Kit for the 23/24 Season',
+        'description': 'Manchester United Away Kit for the 24/25 Season',
         'quantity': '10',
         'sold': '7'
     }

7867bd3cd6f7d232e5a583d8cf821f3b071d5443
Ragnall16 <ragnall.malfath@gmail.com>
2024-09-10 14:02:54 +0700

    added name, npm, and class

--- main/views.py
+++ main/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
+        'app_name' : 'Gopedia',
+        'nama' : 'Ragnall Muhammad Al Fath',
+        'npm' : '2306210550',
+        'kelas' : 'PBP C',
         'name' : '24/25 Away Kit',
         'price': '1000000',
         'description': 'Manchester United Away Kit for the 24/25 Season',
